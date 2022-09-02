8f94bae8ee77ebc4589c9f409aa1948a363158d5
Emilio <emil42x2@stud.kea.dk>
2022-09-02 19:20:01 +0200

    rebuilt data fetch

--- scripts.js
+++ scripts.js
@@ -1,28 +1,15 @@
 const dataSource = "https://kea-alt-del.dk/t7/api/products/2801"
 
-fetch(dataSource)
-    .then((res) => res.json())
-    .then((product) => displayProduct(product))
-    .then(console.log("Successfully fetched data."))
+fetch("https://kea-alt-del.dk/t7/api/products/2801").then(function (response) {
+    return response.json()
+}).then(function (obj) {
+    console.log(obj)
+}).catch(function (error) {
+    console.error("Failed to fetch data:", error)
+})
 
-function displayProduct(product) {
-    console.log("Displaying ", product)
-    document.querySelector(".itemName")
-}
+// IMPORTING TEMPLATES
 
-// const structureTemplate = document.getElementById("listedItem")
-// const templateClone = structureTemplate.cloneNode(true)
-
-let listedItem = document.querySelector("#listeditemtemp".content)
-let listedItemClone = listedItem.cloneNode(true)
-listedItemClone.querySelector("#itemdescription").textContent = product.price
-
-// let listedProperty
-
-// function insertInfo(itemInfo, listedProperty){
-//     document.getElementsByClassName(itemInfo).textContent = listedProperty
-//     console.log("The ", itemInfo, " field has been populated with the data found in the ", listedProperty, "property of the item information datasheet.")
-
-// }
-
-// insertInfo("itemName", "product.variantname")
+const bct1 = document.getElementsByName("#breadcrumbtemp1")
+bct1 => bct1.cloneNode(true)
+bct1Clone.querySelector("ul").innerHTML = "<li>Home</li><li>`${datasource.brand}`</li>"
