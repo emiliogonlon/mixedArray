4e044766183fa59cf3e4eed57c879700b55a5e73
Emilio <emil42x2@stud.kea.dk>
2022-09-02 23:43:54 +0200

    .

--- scripts.js
+++ scripts.js
@@ -8,8 +8,33 @@ fetch("https://kea-alt-del.dk/t7/api/products/2801").then(function (response) {
     console.error("Failed to fetch data:", error)
 })
 
+const jCategory = dataSource.category
+
 // IMPORTING TEMPLATES
 
-const bct1 = document.getElementsByName("#breadcrumbtemp1")
-bct1 => bct1.cloneNode(true)
-bct1Clone.querySelector("ul").innerHTML = "<li>Home</li><li>`${datasource.brand}`</li>"
+// function insertCategory (){
+//     let catClone = document.getElementById("bc1").cloneNode(true)
+//     catClone.textContent = jCategory
+//     document.getElementById("bc1").parentElement.appendChild(catClone)
+// }
+
+// let bcta = document.getElementById("breadcrumbtempl").childNodes[1]
+// let bctb = bcta.cloneNode(true)
+
+// bctb.getElementById("bc1").textContent = `${dataSource.category}`
+// bctb.getElementById("bc2").textContent = `${dataSource.gender}`
+// bctb.getElementById("bc3").textContent = `${dataSource.season}`
+
+// document.querySelector("#breadcrumb").appendChild(bctb) 
+
+const ul = document.getElementById("bcul")
+
+const fragment = new DocumentFragment()
+
+for (const char of dataSource) {
+    const li = document.createElement("li")
+    li.textContent = char
+    fragment.append(li)
+}
+
+ul.append(fragment)
